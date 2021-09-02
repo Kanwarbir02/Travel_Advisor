52e583eef7e9cdd000ff415e96e87027ae87a337
Kanwarbir02 <kanwarbir2002@gmail.com>
2021-09-02 13:34:41 +0530

    site structural divider

--- src/App.js
+++ src/App.js
@@ -2,12 +2,25 @@ import Header from "./components/Header/Header";
 import List from "./components/List/List";
 import Map from "./components/Maps/Map";
 
+import { CssBaseline, Grid } from "@material-ui/core";
+
 const App = () =>  {
   return (
     <div>
-      <Header />
-      <List />
-      <Map />
+      <CssBaseline />
+        <Header />
+      <Grid container spacing={4} style={{width: "100%"}}>
+        <Grid item xs={12} md={4} style={{background:"blue"}}>
+            <List />
+        </Grid>
+
+        <Grid item xs={12} md={8} style={{background:"yellow"}}>
+          <Map />
+        </Grid>
+
+      </Grid>  
+        
+        
     </div>
     
 
