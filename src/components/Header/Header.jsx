1467fc5cd3069a02a7903440e3dd1515cefcb6d4
Kanwarbir02 <kanwarbir2002@gmail.com>
2021-09-02 15:59:04 +0530

    Little styling issue resolved

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,31 +1,37 @@
 import { Autocomplete } from "@react-google-maps/api";
 import { AppBar, Toolbar, Typography, InputBase, Box } from "@material-ui/core";
 import SearchIcon from "@material-ui/icons/Search";
+import useStyles from "./styles";
 
 const Header = () => {
+
+    const styles = useStyles()
+
     return ( 
-        <AppBar position="static">
-            <Toolbar>
-                <Typography variant="h4">
+        <AppBar position="static" >
+            <Toolbar className={styles.toolbar}>
+                <Typography variant="h5" className={styles.title}>
                     Travel Advisor
                 </Typography>
-            </Toolbar>
+            
 
             <Box display="flex">
-                <Typography variant="h5">
+                <Typography variant="h6" className={styles.title}>
                     Explore
                 </Typography>
 
-                <div style={{display:"flex"}}>
-                    <div>
-                        <div>
+                {/* <div> */}
+                    <div className={styles.search}>
+                        <div className={styles.searchIcon}>
                             <SearchIcon />
                         </div>
                     </div>
 
-                    <InputBase placeholder="Search..." />
-                </div>
+                    <InputBase placeholder="Search..." classes={{ root: styles.inputRoot, input: styles.inputInput }}/>
+                {/* </div> */}
             </Box>
+
+            </Toolbar>
         </AppBar>
      );
 }
