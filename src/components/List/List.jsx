9fcdb01bd7791eb14c7c988ae4bd6cc484faf0cb
Kanwarbir02 <kanwarbir2002@gmail.com>
2021-09-02 20:51:04 +0530

    Added state and values

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,28 +1,39 @@
 import { Typography, FormControl, InputLabel, Select, MenuItem } from "@material-ui/core";
+import { useState } from "react";
 import useStyles from "./styles"
 
 const List = () => {
+
+    const [type, setType] = useState("restaurants");
+    const [rating, setRating] = useState("");
 
     const styles = useStyles();
 
     return ( 
         <div className={styles.container}>
             <Typography variant="h4">Travel Destinations </Typography>
+
                 <FormControl className={styles.formControl}>
+
                     <InputLabel>Type</InputLabel>
-                    <Select >
-                        <MenuItem>Restaraunts</MenuItem>
-                        <MenuItem>Hotels</MenuItem>
-                        <MenuItem>Tourist Spots</MenuItem>
+
+                    <Select value={type} onChange={(e)=> setType(e.target.value)}>
+                        <MenuItem value="restaurants">Restaurants</MenuItem>
+                        <MenuItem value="hotels">Hotels</MenuItem>
+                        <MenuItem value="tourSpots">Tourist Spots</MenuItem>
+                        {console.log(type)}
                     </Select>
+
                 </FormControl>
 
                 <FormControl className={styles.formControl}>
-                    <InputLabel>Above</InputLabel>
-                    <Select>
-                        <MenuItem>Above 3.0</MenuItem>
-                        <MenuItem>Above 4.0</MenuItem>
-                        <MenuItem>Above 5.0</MenuItem>
+
+                    <InputLabel>Rating</InputLabel>
+
+                    <Select value={rating} onChange={(e)=> setRating(e.target.value)}>
+                        <MenuItem value={3}>Above 3.0</MenuItem>
+                        <MenuItem value={4}>Above 4.0</MenuItem>
+                        <MenuItem value={4.5}>Above 4.5</MenuItem>
                     </Select>
                 </FormControl>
             
