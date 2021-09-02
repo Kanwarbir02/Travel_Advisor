377465792d594124425385810cbdba58bcc9a37d
Kanwarbir02 <kanwarbir2002@gmail.com>
2021-09-02 20:35:54 +0530

    List fronts

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,7 +1,31 @@
+import { Typography, FormControl, InputLabel, Select, MenuItem } from "@material-ui/core";
+import useStyles from "./styles"
+
 const List = () => {
+
+    const styles = useStyles();
+
     return ( 
-        <div>
-            <h1>List</h1>
+        <div className={styles.container}>
+            <Typography variant="h4">Travel Destinations </Typography>
+                <FormControl className={styles.formControl}>
+                    <InputLabel>Type</InputLabel>
+                    <Select >
+                        <MenuItem>Restaraunts</MenuItem>
+                        <MenuItem>Hotels</MenuItem>
+                        <MenuItem>Tourist Spots</MenuItem>
+                    </Select>
+                </FormControl>
+
+                <FormControl className={styles.formControl}>
+                    <InputLabel>Above</InputLabel>
+                    <Select>
+                        <MenuItem>Above 3.0</MenuItem>
+                        <MenuItem>Above 4.0</MenuItem>
+                        <MenuItem>Above 5.0</MenuItem>
+                    </Select>
+                </FormControl>
+            
         </div>
      );
 }
