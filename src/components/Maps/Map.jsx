81790c8cb43dc7cbebd0ced035673fa535ccbf8b
Kanwarbir02 <kanwarbir2002@gmail.com>
2021-09-02 18:32:57 +0530

    Maps components ready

--- src/components/Maps/Map.jsx
+++ src/components/Maps/Map.jsx
@@ -1,7 +1,28 @@
+import GoogleMapReact from "google-map-react";
+import { Paper, Typography, UseMediaQuery } from "@material-ui/core";
+import { Rating } from "@material-ui/lab";
+import LocationOutlinedIcon from "@material-ui/icons";
+
 const Map = () => {
+
+    //For the instances on a mobile 
+    const isMobile = UseMediaQuery("(min-width: 600px)")
+
+    const coordinates = {lat: 0, lng: 0}
+
     return ( 
         <div>
-            <h1>Map</h1>
+            <GoogleMapReact 
+                bootstrapURLKeys= {keys= ""} 
+                defaultCenter={coordinates}
+                center={coordinates} 
+                defaultZoom={14} 
+                margin={[50,50,50,50]} 
+                options={""} 
+                onChange={""}
+                onChildClick={""}  >
+
+            </GoogleMapReact>
         </div>
      );
 }
