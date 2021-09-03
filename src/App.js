0ae031d0dcd77e2934b477a1ab159506a493c78b
Kanwarbir02 <kanwarbir2002@gmail.com>
2021-09-03 13:54:25 +0530

    Get request correction

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ const App = () =>  {
         console.log(data);
         setPlaces(data);
       })
-  })
+  }, [])
 
   return (
     <div>
--- src/api/apiDetails.js
+++ src/api/apiDetails.js
@@ -1,21 +1,14 @@
 export const Options = {
-    method: 'GET',
-    url: 'https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary',
-    params: {
-      bl_latitude: '11.847676',
-      tr_latitude: '12.838442',
-      bl_longitude: '109.095887',
-      tr_longitude: '109.149359',
-      restaurant_tagcategory_standalone: '10591',
-      restaurant_tagcategory: '10591',
-      limit: '30',
-      currency: 'USD',
-      open_now: 'false',
-      lunit: 'km',
-      lang: 'en_US'
-    },
-    headers: {
-      'x-rapidapi-host': 'travel-advisor.p.rapidapi.com',
-      'x-rapidapi-key': '8cb5d27519msha697911e80d08b5p106054jsncc8ca630eaab'
-    }
+
+  params: {
+    bl_latitude: '11.847676',
+    tr_latitude: '12.838442',
+    bl_longitude: '109.095887',
+    tr_longitude: '109.149359',
+    
+  },
+  headers: {
+    'x-rapidapi-host': 'travel-advisor.p.rapidapi.com',
+    'x-rapidapi-key': '8cb5d27519msha697911e80d08b5p106054jsncc8ca630eaab'
+  }
 };
